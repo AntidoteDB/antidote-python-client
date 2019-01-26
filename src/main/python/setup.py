cbfe4307ff5756ef379a89915cead82043db6f9a
preguica <nmp@fct.unl.pt>
2019-01-26 15:58:27 +0000

    First runnable version

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -8,17 +8,16 @@ Created on Fri Jan 25 21:36:45 2019
 
 import setuptools
 
-#with open("README.md", "r") as fh:
-#    long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="antidotedb",
-    version="0.0.2",
+    version="0.1",
     author="Nuno Preguica",
     author_email="nuno.preguica@fct.unl.pt",
     description="AntidoteDB Python clients",
-#    long_description=long_description,
-    long_description="bla",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/AntidoteDB/antidote-python-client/tree/master/src/main/python/antidotedb",
     packages=['antidotedb','antidotedb.proto'],

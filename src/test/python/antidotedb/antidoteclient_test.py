d37103fe9d0412e92685684dea019e39ae19fe3d
Peter Zeller <p_zeller@cs.uni-kl.de>
2022-01-29 21:22:44 +0100

    fix tests and update proto file to latest version

--- src/test/python/antidotedb/antidoteclient_test.py
+++ src/test/python/antidotedb/antidoteclient_test.py
@@ -41,8 +41,8 @@ def test_counter( server = "localhost", port = 8087) :
     return res
 
 def test_fatcounter(server = "localhost", port = 8087) :
-    key = Key( "some_bucket", "some_key_counter", "FATCOUNTER")
-    key2 = Key( "some_bucket", "some_other_key_counter", "FATCOUNTER")
+    key = Key( "some_bucket", "some_key_fatcounter", "FATCOUNTER")
+    key2 = Key( "some_bucket", "some_other_key_fatcounter", "FATCOUNTER")
     
     clt = AntidoteClient(server,port)
     tx = clt.start_transaction()
@@ -161,9 +161,9 @@ def test_rwset(server = "localhost", port = 8087) :
     tx = clt.start_transaction()
     res = tx.read_objects( key)
     assert( type(res[0]) == Set)
-    res = tx.update_objects( Set.addKeyOp( [val1,val2], key))
+    res = tx.update_objects( Set.AddOp( key, [val1,val2]))
     assert( res)
-    res = tx.update_objects( Set.addKeyOp( val3, key))
+    res = tx.update_objects( Set.AddOp( key, val3))
     assert( res)
     res = tx.read_objects( key)
     assert( type(res[0]) == Set)
@@ -178,7 +178,7 @@ def test_rwset(server = "localhost", port = 8087) :
     assert( val1 in res[0].values())
     assert( val2 in res[0].values())
     assert( val3 in res[0].values())
-    res = tx.update_objects( Set.removeKeyOp( val2, key))
+    res = tx.update_objects( Set.RemoveOp(key, val2))
     assert( res)
     res = tx.read_objects( key)
     assert( type(res[0]) == Set)
@@ -259,10 +259,6 @@ def test_gmap(server = "localhost", port = 8087) :
     res = tx.read_objects( key)
     assert( k1 in res[0].values())
     assert( k2 in res[0].values())
-
-    res = tx.update_objects( Map.RemoveOp( key, [Key( "", k1, "COUNTER")]))
-    assert( res == False)
-
     return res
 
 def test_rrmap(server = "localhost", port = 8087) :
